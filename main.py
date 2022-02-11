0a838c3be60a4a311368bd7a4a4707d1782a1a95
Vladimir Belich <belichvladimir@mail.ru>
2022-02-11 18:12:18 +0300

    Homework3

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pprint import pprint
 class Superheroapi:
     def __init__(self,token):
         self.token = token
-    pass
     def superhero_get_info(self,name):
         url = f'https://superheroapi.com/api/{self.token}/search/{name}'
         res = requests.get(url)
@@ -15,8 +14,8 @@ class Superheroapi:
             if self.superhero_get_info(name)> int_hero:
                 int_hero = self.superhero_get_info(name)
                 int_name_hero = name
-            print(f'Герой: {name} интеллект:{self.superhero_get_info(name)}')
-        print(f'\nСамый умный герой: {int_name_hero}, интеллект: {int_hero}')
+            print(f'Hero name: {name} intelligence:{self.superhero_get_info(name)}')
+        print(f'\nMost intelligent hero: {int_name_hero}, intelligence: {int_hero}')
 
 class YaUploader:
     def __init__(self, token: str):
@@ -37,14 +36,31 @@ class YaUploader:
             if res.status_code == 201:
                 print(f'File {file} success upload')
 
+class StackUploader:
+    def get_list_id(self, name_lang):
+        params = {'fromdate': '1644364800', 'creation':'1644537600', 'order':'desc','sort':'activity','site':'stackoverflow'}
+        url = 'https://api.stackexchange.com/2.3/questions'
+        res = requests.get(url, params=params)
+        data = res.json()
+        name = False
+        for line in data['items']:
+            if name_lang.lower() in line['tags'] or name_lang.capitalize() in line['tags']:
+                print(f'Questions with the {name_lang} Tag: "{line["title"]}"')
+                name = True
+        return f'No questions with the {name_lang} Tag' if name == False else print()
+
 if __name__ == '__main__':
     tokenYA = 'AQAAAAAB81f3AADLWzYPyYZ01kzYvRRoDeRxico'
     tokenSH = '2619421814940190'
     path_to_file = ['yauploader.txt','yauploder2.txt']
-    print('\nHomework1\n')
+    print('\nHomework:1\n')
     hero = Superheroapi(tokenSH)
     hero.compare_hero(['Hulk','Captain America', 'Thanos'])
-    print('\nHomework2\n')
+    print('\nHomework:2\n')
     print( 'List of file to upload:',', '.join(path_to_file))
     uploader = YaUploader(tokenYA)
     result = uploader.upload(path_to_file)
+    print('\nHomework:3\n')
+    stack = StackUploader()
+    stack.get_list_id('Python')
+
