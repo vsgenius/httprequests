e9c723c2d44768f4f61cd16ca8f617a096f3b655
Vladimir Belich <belichvladimir@mail.ru>
2022-02-11 16:39:19 +0300

    Homework2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from pprint import pprint
-class superheroapi:
+class Superheroapi:
     def __init__(self,token):
         self.token = token
     pass
@@ -18,6 +18,33 @@ class superheroapi:
             print(f'Герой: {name} интеллект:{self.superhero_get_info(name)}')
         print(f'\nСамый умный герой: {int_name_hero}, интеллект: {int_hero}')
 
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {'Content-Type': 'application/json','Authorization': f'OAuth {self.token}'}
+
+    def get_href(self,path):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload/'
+        res=requests.get(url, headers=self.get_headers(), params={'path': path,'overwrite':True})
+        return res.json()['href']
+
+    def upload(self, file_path: str):
+        for file in file_path:
+            path = self.get_href(f'/Netology/{file}')
+            res = requests.put(path, open(file,'rb'), headers=self.get_headers(),)
+            if res.status_code == 201:
+                print(f'File {file} success upload')
+
 if __name__ == '__main__':
-    hero = superheroapi('2619421814940190')
+    tokenYA = 'AQAAAAAB81f3AADLWzYPyYZ01kzYvRRoDeRxico'
+    tokenSH = '2619421814940190'
+    path_to_file = ['yauploader.txt','yauploder2.txt']
+    print('\nHomework1\n')
+    hero = Superheroapi(tokenSH)
     hero.compare_hero(['Hulk','Captain America', 'Thanos'])
+    print('\nHomework2\n')
+    print( 'List of file to upload:',', '.join(path_to_file))
+    uploader = YaUploader(tokenYA)
+    result = uploader.upload(path_to_file)
